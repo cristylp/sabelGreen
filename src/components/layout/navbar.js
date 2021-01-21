99197857cc43482a79dbfc2c0466dcdffa62bc2c
Cristy Lopez Piñeiro <aclopezp93@gmail.com>
2021-01-21 23:11:41 +0100

    first commit

---
+++ src/components/layout/navbar.js
@@ -0,0 +1,35 @@
+import React from "react"
+import "./navbar.css"
+
+import { Navbar, Nav } from 'react-bootstrap'
+import logo from './../images/logogris.png'
+
+
+export default function Navigation() {
+    return (
+        <>
+            <Navbar sticky="top" className='color-navbar' variant='light' expand='lg'>
+                <Navbar.Brand href="#home">
+                    <img
+                        src={logo}
+                        width="330"
+                        height="100"
+                        className="d-inline-block align-top"
+                        alt="Sabel Green logo"
+                    />
+                </Navbar.Brand>
+                <Navbar.Toggle />
+                <Navbar.Collapse className="justify-content-end">
+                    <Nav>
+                        <Nav.Link className="font" href="#">Sabel Green</Nav.Link>
+                        <Nav.Link className="font" href="#">Mercados</Nav.Link>
+                        <Nav.Link className="font" href="#">Áreas de negocio</Nav.Link>
+                        <Nav.Link className="font" href="#">Equipo</Nav.Link>
+                        <Nav.Link className="font" href="#">Impacto</Nav.Link>
+                        <Nav.Link className="font" href="#">Contacto</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+        </>
+    )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,29 @@
 import React from "react"
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import Navigation from "../components/layout/navbar"
+import Header from "../components/banner"
+import Cards from "../components/infoCards"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    
+    <>
+      <div>
+        <Navigation />
+      </div>
+    
+     <div>
+        <Header />
+      </div>
+
+      <div>
+        <Cards />
+      </div>
+
+    </>
+
+  
+    
+  )
 }
