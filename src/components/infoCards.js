e131ad9de1d8e2ecd65db93c90e22447c784f883
Cristy Lopez Piñeiro <aclopezp93@gmail.com>
2021-01-22 12:19:40 +0100

    second commit

--- src/components/infoCards.js
+++ src/components/infoCards.js
@@ -1,7 +1,7 @@
 import React from "react"
 import "./infoCards.css"
 
-import { Card, Col } from 'react-bootstrap'
+import { Card, Col, Row } from 'react-bootstrap'
 import hands from "./../components/images/hands.png"
 import battery from "./../components/images/battery.png"
 import construction from "./../components/images/construction.png"
@@ -11,38 +11,42 @@ import world from "./../components/images/world.png"
 export default function Cards() {
     return (
         <>
-            <Col xs={2} md={4}>
-                <Card className='info-card' style={{ width: '35rem' }}>
-                    <div>
-                        <Card.Img variant="top" src={hands} />
-                        <Card.Body>
-                            <Card.Title>+50 años</Card.Title>
-                            <Card.Text>de experiencia agregada</Card.Text>
-                        </Card.Body>
-                    </div>
-                    <div>
-                        <Card.Img variant="top" src={battery} />
-                        <Card.Body>
-                            <Card.Title>+500 MW</Card.Title>
-                            <Card.Text>en desarrollo</Card.Text>
-                        </Card.Body>
-                    </div>
-                    <div>
-                        <Card.Img variant="top" src={construction} />
-                        <Card.Body>
-                            <Card.Title>+10 proyectos</Card.Title>
-                            <Card.Text>en marcha</Card.Text>
-                        </Card.Body>
-                    </div>
-                    <div>
-                        <Card.Img variant="top" src={world} />
-                        <Card.Body>
-                            <Card.Title>Internacional</Card.Title>
-                            <Card.Text>con oficinas en España</Card.Text>
-                        </Card.Body>
-                    </div>
-                </Card>
-            </Col>
+            <div className='bg-color'>
+                <Row>
+                    <Col md={{ span: 4, offset: 4 }}>
+                        <Card className='info-card' style={{ width: '40rem' }}>
+                            <div>
+                                <Card.Img variant="top" src={hands} />
+                                <Card.Body>
+                                    <Card.Title>+50 años</Card.Title>
+                                    <Card.Text>de experiencia agregada</Card.Text>
+                                </Card.Body>
+                            </div>
+                            <div>
+                                <Card.Img variant="top" src={battery} />
+                                <Card.Body>
+                                    <Card.Title>+500 MW</Card.Title>
+                                    <Card.Text>en desarrollo</Card.Text>
+                                </Card.Body>
+                            </div>
+                            <div>
+                                <Card.Img variant="top" src={construction} />
+                                <Card.Body>
+                                    <Card.Title>+10 proyectos</Card.Title>
+                                    <Card.Text>en marcha</Card.Text>
+                                </Card.Body>
+                            </div>
+                            <div>
+                                <Card.Img variant="top" src={world} />
+                                <Card.Body>
+                                    <Card.Title>Internacional</Card.Title>
+                                    <Card.Text>con oficinas en <br /> España y México</Card.Text>
+                                </Card.Body>
+                            </div>
+                        </Card>
+                    </Col>
+                 </Row>
+            </div>
         </>
     )
 }
