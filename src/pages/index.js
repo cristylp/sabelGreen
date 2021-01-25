dcc9ca109f1797dcf512ae4ecacf724dc1305f86
isa lopez <ilopezg31@gmail.com>
2021-01-25 16:48:06 +0100

    contact form almost finished

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,49 +10,34 @@ import HeaderFour from "../components/banner4"
 import HeaderFive from "../components/banner5"
 import CardsTwo from "../components/icons2"
 import HeaderSix from "../components/banner6"
-
+import Contactform from "../components/contactform"
 
 export default function Home() {
   return (
 
     <>
-      <div>
+
         <Navigation />
-      </div>
 
-     <div>
         <Header />
-      </div>
 
-      <div>
         <Cards />
-      </div>
 
-      <div>
         <HeaderTwo />
-      </div>
 
-      <div>
         <HeaderTree />
-      </div>
 
-      <div>
         <HeaderFour />
-      </div>
 
-      <div>
         <HeaderFive />
-      </div>
 
-      <div>
         <CardsTwo />
-      </div>
 
-      <div>
         <HeaderSix />
-      </div>
+
+        <Contactform/>
 
     </>
-    
+
   )
 }
